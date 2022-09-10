ccf02ffab870a5f2346030d0fe47ab4d4e540d83
MatweyL <matweyka2003@mail.ru>
2022-09-10 11:39:20 +0300

    added likes getting methods

--- profile_analizer.py
+++ profile_analizer.py
@@ -2,7 +2,7 @@ from re import S
 import vk
 
 from dotenv_utils import get_access_token
-from utils import print_access_token_getting_url
+from utils import print_access_token_getting_url, print_object
 
 class ProfileAnalizer:
 
@@ -12,6 +12,9 @@ class ProfileAnalizer:
 
 
     def resolve_screen_name(self, screen_name):
+        """
+        return {'object_id': 12345678, 'type': 'user'}
+        """
         return self.vk_api.utils.resolveScreenName(screen_name=screen_name)
 
     def is_user_profile_accessable(self, user_id):
@@ -21,14 +24,59 @@ class ProfileAnalizer:
             user_info["is_closed"] == True and user_info['is_friend'] == 0):
             return False
         return True
+    
+    def get_wall_media(self, user_id, count=100):
+        items = self.vk_api.wall.get(owner_id=user_id, count=count)
+        return items
 
+    def get_wall_liked_users(self, user_id, count):
+        liked_users = {}
+        wall_media = self.get_wall_media(user_id, count=count)
+        for post in wall_media['items']:
+            current_post_liked_users = self.vk_api.likes.getList(type=post['post_type'], owner_id=user_id, item_id=post['id'])['items']
+            for user in current_post_liked_users:
+                if liked_users.get(user):
+                    liked_users[user][0] += 1
+                    liked_users[user][1].append(post['id'])
+                else:
+                    liked_users[user] = [1, post['id']]
+        return liked_users
+
+    def get_photos(self, user_id, count=100):
+        photos = self.vk_api.photos.getAll(owner_id=user_id, count=count)
+        return photos
+
+    def get_photos_liked_users(self, user_id, count=100):
+        liked_users = {}
+        photos = self.get_photos(user_id, count=count)
+        for photo in photos['items']:
+            current_post_liked_users = self.vk_api.likes.getList(type=photo, owner_id=user_id, item_id=photo['id'])['items']
+            for user in current_post_liked_users:
+                if liked_users.get(user):
+                    liked_users[user][0] += 1
+                    liked_users[user][1].append(photo['id'])
+                else:
+                    liked_users[user] = [1, photo['id']]
+        return liked_users
+                
+
+"""    id:
+        457242927
+    owner_id:
+        140351546
+    post_id:
+        431"""
 
 
 if __name__ == "__main__":
     try:
         pa = ProfileAnalizer(get_access_token())
-        user_id = pa.resolve_screen_name("id431906877")['object_id']
-        print(pa.is_user_profile_accessable(user_id))
+        user_id = pa.resolve_screen_name("crave_ozer_man")['object_id']
+        # print_object(pa.get_photos(user_id, count=1))
+        print_object(pa.get_wall_media(user_id, count=4))
+        # print(pa.is_user_profile_accessable(user_id))
     except vk.exceptions.VkAPIError as e:
         print(e)
         print_access_token_getting_url()
+
+
--- utils.py
+++ utils.py
@@ -7,3 +7,27 @@ from dotenv_utils import get_api_id
 
 def print_access_token_getting_url():
     print(f"https://oauth.vk.com/authorize?client_id={get_api_id()}&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope[]=friends&scope[]=photos&response_type=token&v=5.131")
+
+
+def print_object(obj, offset=0):
+    if isinstance(obj, str) or isinstance(obj, int) or isinstance(obj, float) or isinstance(obj, str):
+        print(f"{offset * '    '}{obj}")
+    elif isinstance(obj, dict):
+        print(f"{offset * '    '}", "{", sep='')
+        for key in obj:
+            print(f"{offset * '    '}{key}:")
+            print_object(obj[key], offset + 1)
+        print(f"{offset * '    '}", "}", sep='')
+    elif isinstance(obj, list):
+        print(f"{offset * '    '}", "[", sep='')
+        for elem in obj:
+            print_object(elem, offset)
+        print(f"{offset * '    '}", "]", sep='')
+    elif isinstance(obj, set) or isinstance(obj, tuple):
+        print(f"{offset * '    '}", "(", sep='')
+        for elem in obj:
+            print_object(elem, offset)
+        print(f"{offset * '    '}", ")", sep='')
+    else:
+        print(f"{offset * '    '}{obj}")
+        
